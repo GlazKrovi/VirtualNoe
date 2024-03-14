682a1e9ce5dc604ca442b87097d8e8904e5f9e8e
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2024-03-14 13:17:57 +0100

    update refresh system

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $creatures = \App\Models\Creature::all();
+            foreach ($creatures as $creature) {
+                $controller = new \App\Http\Controllers\ClockController();
+                $controller->refresh($creature->id);
+            }
+        })->everyFiveSeconds();
     }
 
     /**
@@ -20,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Http/Controllers/ClockController.php
+++ app/Http/Controllers/ClockController.php
@@ -7,18 +7,43 @@ use Carbon\Carbon;
 
 class ClockController extends Controller
 {
+    private $REFRESH_COOLDOWN = 2;
+
+    /**
+     * Every 2 minutes, make specified creature hungry, or hurt her if hunger level is 0.
+     *
+     * @param integer $creatureId
+     * @return void
+     */
     public function refresh(int $creatureId)
     {
         $creature = Creature::find($creatureId);
         $currentTime = Carbon::now();
         $lastUpdate = Carbon::parse($creature->updated_at);
         $differenceInMinutes = $currentTime->diffInMinutes($lastUpdate);
-        if ($differenceInMinutes >= 5) {
+        if ($differenceInMinutes >= $this->REFRESH_COOLDOWN) {
             if ($creature->hunger() <= 0) {
                 $creature->makeHungry(10);
             } else {
                 $creature->hurt(10);
             }
+        }
+    }
+
+    /**
+     * Make specified creature hungry, or hurt her if hunger level is 0.
+     * Same as refresh() but without cooldown.
+     *
+     * @param integer $creatureId
+     * @return void
+     */
+    public function refreshFast(int $creatureId)
+    {
+        $creature = Creature::find($creatureId);
+        if ($creature->hunger() <= 0) {
+            $creature->makeHungry(10);
+        } else {
+            $creature->hurt(10);
         }
     }
 }
