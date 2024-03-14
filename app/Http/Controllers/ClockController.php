ad805eafd5f600ffcfcef6291a09f8f940f93264
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2024-03-14 13:26:29 +0100

    remove useless function

--- app/Http/Controllers/ClockController.php
+++ app/Http/Controllers/ClockController.php
@@ -3,38 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Creature;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 
 class ClockController extends Controller
 {
-    private $REFRESH_COOLDOWN = 2;
-
-    /**
-     * Every 2 minutes, make specified creature hungry, or hurt her if hunger level is 0.
-     *
-     * @param integer $creatureId
-     * @return void
-     */
-    public function refresh(int $creatureId)
-    {
-        $creature = Creature::find($creatureId);
-        $currentTime = Carbon::now();
-        $lastUpdate = Carbon::parse($creature->updated_at);
-        $differenceInMinutes = $currentTime->diffInMinutes($lastUpdate);
-        if ($differenceInMinutes >= $this->REFRESH_COOLDOWN) {
-            if ($creature->hunger() <= 0) {
-                $creature->makeHungry(10);
-            } else {
-                $creature->hurt(10);
-            }
-        }
-
-        Log::debug("refresh called");
-
-        // Update the timestamp of the last update
-        $creature->touch();
-    }
-
     /**
      * Make specified creature hungry, or hurt her if hunger level is 0.
      * Same as refresh() but without cooldown.
